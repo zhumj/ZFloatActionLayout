a78bc55f28515fd57e2795ee0398d07b44fa793b
zhumj <421789838@qq.com>
2019-10-10 02:00:50 -0700

    优化控件

--- app/src/main/java/com/zhumj/zfloatactionlayout/ZFloatActionLayout.kt
+++ app/src/main/java/com/zhumj/zfloatactionlayout/ZFloatActionLayout.kt
@@ -10,17 +10,17 @@ import android.view.animation.LinearInterpolator
 import android.widget.RelativeLayout
 import android.os.CountDownTimer
 
-
-
-
 class ZFloatActionLayout(context: Context?, attrs: AttributeSet?) : RelativeLayout(context, attrs) {
 
-    //是否开启X轴吸边
-    private var isAdsorbX: Boolean = false
+    //吸边模式
+    //none(0): 默认模式，不开启吸边
+    //both(1): X轴和Y轴同时吸边
+    //unilateral(2): 单边模式，X轴和Y轴哪个轴距离边界近就吸哪个轴
+    //adsorbX(3): 只有X轴吸边
+    //adsorbY(4): 只有Y轴吸边
+    private var adsorbMode: Int = 0
     //X轴吸边之后与边缘的距离
     private var adsorbXMargin: Float = 0f
-    //是否开启Y轴吸边
-    private var isAdsorbY: Boolean = false
     //Y轴吸边之后与边缘的距离
     private var adsorbYMargin: Float = 0f
     //是否开启半隐藏
@@ -32,23 +32,22 @@ class ZFloatActionLayout(context: Context?, attrs: AttributeSet?) : RelativeLayo
 
     private var parentHeight: Int = 0//父布局高度
     private var parentWidth: Int = 0//父布局宽度
-    private var lastX: Int = 0
-    private var lastY: Int = 0
 
     private var isDrag: Boolean = false//是否滑动
     private var isHided: Boolean = false//记录是否已半隐藏
     private var isHiding: Boolean = false//正在隐藏
     private var isShowing: Boolean = false//正在显示
-    private var isHideX: Boolean = false//X轴半隐藏
-    private var isHideY: Boolean = false//Y轴半隐藏
+    private var isHideX: Boolean = false//判断X轴是否半隐藏
+    private var isHideY: Boolean = false//判断Y轴是否半隐藏
 
-    private var timer: CountDownTimer? = null
+    private var timer: CountDownTimer? = null//倒计时开启半隐藏
+    private var lastX: Int = 0
+    private var lastY: Int = 0
 
     init {
         val array = context?.obtainStyledAttributes(attrs, R.styleable.ZFloatActionLayout)
-        isAdsorbX = (array?.getBoolean(R.styleable.ZFloatActionLayout_ZFloatActionLayout_isAdsorbX, isAdsorbX)) ?: isAdsorbX
+        adsorbMode = array?.getInt(R.styleable.ZFloatActionLayout_ZFloatActionLayout_adsorbMode, 0) ?: 0
         adsorbXMargin = (array?.getDimension(R.styleable.ZFloatActionLayout_ZFloatActionLayout_adsorbXMargin, adsorbXMargin)) ?: adsorbXMargin
-        isAdsorbY = (array?.getBoolean(R.styleable.ZFloatActionLayout_ZFloatActionLayout_isAdsorbY, isAdsorbY)) ?: isAdsorbY
         adsorbYMargin = (array?.getDimension(R.styleable.ZFloatActionLayout_ZFloatActionLayout_adsorbYMargin, adsorbYMargin)) ?: adsorbYMargin
         isHalfHidden = (array?.getBoolean(R.styleable.ZFloatActionLayout_ZFloatActionLayout_isHalfHidden, isHalfHidden)) ?: isHalfHidden
         displayDuration = (array?.getInt(R.styleable.ZFloatActionLayout_ZFloatActionLayout_displayDuration, displayDuration)) ?: displayDuration
@@ -119,23 +118,24 @@ class ZFloatActionLayout(context: Context?, attrs: AttributeSet?) : RelativeLayo
                     //判断是否为点击事件
                     val hasParent = parentHeight > 0 && parentWidth > 0//只有父布局存在才可以拖动
                     //自动贴边
-                    if (hasParent && isDrag && (isAdsorbX || isAdsorbY)) {
-                        moveToEdge()
-                    } else {//如果没有设置吸边，但设置了可以半隐藏，则X轴或者Y轴到达边缘的时候自动半隐藏
-                        if (isHalfHidden) {
-                            val dx = rawX - lastX
-                            val dy = rawY - lastY
-                            val x = x + dx
-                            val y = y + dy
-                            //检测是否到达边缘 左上右下
-                            isHideX = x <= 0 || x >= parentWidth - width
-                            isHideY = y <= 0 || y >= parentHeight - height
-                            if (isHideX || isHideY) {
-                                startHalfHidden()
+                    if (hasParent && isDrag) {
+                        if (adsorbMode != 0) {
+                            moveToEdge()
+                        } else {//如果没有设置吸边，但设置了可以半隐藏，则X轴或者Y轴到达边缘的时候自动半隐藏
+                            if (isHalfHidden) {
+                                val dx = rawX - lastX
+                                val dy = rawY - lastY
+                                val x = x + dx
+                                val y = y + dy
+                                //检测是否到达边缘 左上右下
+                                isHideX = x <= 0 || x >= parentWidth - width
+                                isHideY = y <= 0 || y >= parentHeight - height
+                                if (isHideX || isHideY) {
+                                    startHalfHidden()
+                                }
                             }
                         }
                     }
-
                     //如果是拖动状态下即非点击按压事件
                     isPressed = !isDrag
                 } else {
@@ -178,8 +178,6 @@ class ZFloatActionLayout(context: Context?, attrs: AttributeSet?) : RelativeLayo
 
                 override fun onAnimationEnd(animation: Animator?) {
                     if (isHalfHidden) {
-                        isHideX = isAdsorbX
-                        isHideY = isAdsorbY
                         startTimer()
                     }
                 }
@@ -188,23 +186,83 @@ class ZFloatActionLayout(context: Context?, attrs: AttributeSet?) : RelativeLayo
 
                 override fun onAnimationStart(animation: Animator?) {}
             })
-        //X轴自动贴边
-        if (isAdsorbX) {
-            val centerX =  parentWidth/ 2
-            adsorbAnim.x(if (lastX < centerX) {//向左贴边
-                adsorbXMargin
-            } else {//向右贴边
-                parentWidth - width - adsorbXMargin
-            })
-        }
-        //Y轴自动贴边
-        if (isAdsorbY) {
-            val centerY =  parentHeight/ 2
-            adsorbAnim.y(if (lastY < centerY){//向上贴边
-                adsorbYMargin
-            } else {//向下贴边
-                parentHeight - height - adsorbYMargin
-            })
+
+        val centerX =  parentWidth/ 2
+        val centerY =  parentHeight/ 2
+        when (adsorbMode) {
+            1 -> {
+                isHideX = true
+                isHideY = true
+
+                //X轴自动贴边
+                adsorbAnim.x(if (x + width/2 < centerX) {//向左贴边
+                    adsorbXMargin
+                } else {//向右贴边
+                    parentWidth - width - adsorbXMargin
+                })
+                //Y轴自动贴边
+                adsorbAnim.y(if (y + height/2 < centerY){//向上贴边
+                    adsorbYMargin
+                } else {//向下贴边
+                    parentHeight - height - adsorbYMargin
+                })
+            }
+            2 -> {
+                val xOffset = if (x + width/2 < centerX){
+                    x + width/2
+                } else {
+                    parentWidth - (x + width/2)
+                }
+
+                val yOffset = if (y + height/2 < centerY){
+                    y + height/2
+                } else {
+                    parentHeight - (y + height/2)
+                }
+
+                if (xOffset < yOffset) {
+                    isHideX = true
+                    isHideY = false
+
+                    adsorbAnim.x(if (x + width/2 < centerX) {//向左贴边
+                        adsorbXMargin
+                    } else {//向右贴边
+                        parentWidth - width - adsorbXMargin
+                    })
+                } else {
+                    isHideX = false
+                    isHideY = true
+
+                    adsorbAnim.y(if (y + height/2 < centerY){//向上贴边
+                        adsorbYMargin
+                    } else {//向下贴边
+                        parentHeight - height - adsorbYMargin
+                    })
+                }
+            }
+            3 -> {
+                isHideX = true
+                isHideY = false
+
+                adsorbAnim.x(if (x + width/2 < centerX) {//向左贴边
+                    adsorbXMargin
+                } else {//向右贴边
+                    parentWidth - width - adsorbXMargin
+                })
+            }
+            4 -> {
+                isHideX = false
+                isHideY = true
+
+                adsorbAnim.y(if (y + height/2 < centerY){//向上贴边
+                    adsorbYMargin
+                } else {//向下贴边
+                    parentHeight - height - adsorbYMargin
+                })
+            }
+            else -> {
+
+            }
         }
         adsorbAnim.start()
     }
@@ -232,19 +290,18 @@ class ZFloatActionLayout(context: Context?, attrs: AttributeSet?) : RelativeLayo
                     isHiding = true
                 }
             })
-        //X轴自动贴边
+
         if (isHideX) {
             val centerX =  parentWidth/ 2
-            halfHiddenAnim.x(if (lastX < centerX) {//左贴边
+            halfHiddenAnim.x(if (x + width/2 < centerX) {//左贴边
                 -(width/2).toFloat()
             } else {//右贴边
                 (parentWidth - width/2).toFloat()
             })
         }
-        //Y轴自动贴边
         if (isHideY) {
             val centerY =  parentHeight/ 2
-            halfHiddenAnim.y(if (lastY < centerY){//上贴边
+            halfHiddenAnim.y(if (y + height/2 < centerY){//上贴边
                 -(height/2).toFloat()
             } else {//下贴边
                 (parentHeight - height/2).toFloat()
@@ -278,19 +335,17 @@ class ZFloatActionLayout(context: Context?, attrs: AttributeSet?) : RelativeLayo
                     isShowing = true
                 }
             })
-        //X轴自动贴边
         if (isHideX) {
             val centerX =  parentWidth/ 2
-            halfHiddenAnim.x(if (lastX < centerX) {//左贴边
+            halfHiddenAnim.x(if (x + width/2 < centerX) {//左贴边
                 adsorbXMargin
             } else {//右贴边
                 parentWidth - width - adsorbXMargin
             })
         }
-        //Y轴自动贴边
         if (isHideY) {
             val centerY =  parentHeight/ 2
-            halfHiddenAnim.y(if (lastY < centerY){//上贴边
+            halfHiddenAnim.y(if (y + height/2 < centerY){//上贴边
                 adsorbYMargin
             } else {//下贴边
                 parentHeight - height - adsorbYMargin
